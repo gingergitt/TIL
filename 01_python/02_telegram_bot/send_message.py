e368d92d3770510ddf24d6f660828e6f0ababacd
ginger <mro8822@gmail.com>
2019-12-13 11:49:10 +0900

    191213 | added  send_message.py
    q
    
    git commit -m 191213

---
+++ 01_python/02_telegram_bot/send_message.py
@@ -0,0 +1,13 @@
+import requests
+from decouple import config
+
+base = 'https://api.telegram.org'
+token = config('TELEGRAM_BOT_TOKEN')
+chat_id = config'CHAT_ID'
+text = 'Friday'
+
+url = f'{base}/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
+
+send_message = requests.get(url)
+
+print(send_message)
